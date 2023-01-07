41d65be2b1ca24532385104491a8406661578f3c
Ali-Sid <59683051+Ali-Sid@users.noreply.github.com>
2023-01-08 02:55:01 +0530

    updated a comment in main,js

--- simpleToDo in JS/main.js
+++ simpleToDo in JS/main.js
@@ -13,7 +13,7 @@ addTask.addEventListener('click', function(){
         paragraph.style.textDecoration = "line-through";
         paragraph.style.color = "gray";
 
-        clickCounter++; //this counter variable tracks the no. of clicks
+        clickCounter++; //this counter variable increments the no. of clicks
         if (clickCounter % 2 === 0) { //This condition is used to perform the second action (by checking the clickCounter) on every second click, % 2 === 0 checks whether the no. of clicks are even and divisible by 2 with no remainder
     // Action to occur on second click goes here
         paragraph.style.textDecoration = ""; //this removes the line-through text-decoration
